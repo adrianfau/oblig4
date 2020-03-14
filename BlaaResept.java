e4f1dfbc76de35f0142fe8f106d994e1e8ce79c7
kingofrock37 <54410453+kingofrock37@users.noreply.github.com>
2020-03-14 19:11:21 +0100

    Create BlaaResept.java

---
+++ BlaaResept.java
@@ -0,0 +1,22 @@
+class BlaaResept extends Resept {
+  //Test
+  public BlaaResept(Legemiddel lm,  Lege ul, int pId, int reit) {
+    super(lm, ul, pId, reit);
+  }
+
+  public String farge() {
+    return "BLAA";
+  }
+
+  public double prisAaBetale() {
+    return legemiddel.hentPris() * 0.25;
+  }
+
+  @Override
+  public String toString() {
+    return "BLAARESEPT\n" + "Farge: " + this.farge() + " Legemiddel: "
+     + legemiddel.toString() + "\n Lege: " + utskrivendeLege.toString() + "\n ReseptId: "
+      + this.hentId() + " PasientId: " + this.hentPasientId() + " Pris aa betale: "
+       + this.prisAaBetale() + " Reit: " + this.hentReit() + "\n";
+  }
+}
